6395ffbc5b6a102a85c23f2a9a1c5aed15301823
Andrei Bogdanov <bagizara@gmail.com>
2025-12-21 15:13:09 +0100

    Базовый pch.h

---
+++ src/pch.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// Стандартные библиотеки C++
+#include <algorithm>
+#include <atomic>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+// C стандартные библиотеки
+#include <cctype>
+#include <cerrno>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
