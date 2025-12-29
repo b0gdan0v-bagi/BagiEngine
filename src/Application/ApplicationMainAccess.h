16102040c9704716b441579a764cb0132b4f0703
Andrei Bogdanov <bagizara@gmail.com>
2025-12-29 10:29:12 +0100

    Дополнил PassKey насследованием

--- src/Application/ApplicationMainAccess.h
+++ src/Application/ApplicationMainAccess.h
@@ -1,30 +1,10 @@
 #pragma once
 
-#include <Core/Utils/PassKey.h>
-
 namespace Core {
     /**
      * Класс-ключ для доступа к методам Application только из main.cpp и классов,
      * которые наследуются от ApplicationMainAccess (например, ApplicationSDLFabric).
      */
     class ApplicationMainAccess {};
-
-    // Forward declaration для ApplicationSDLFabric
-    class ApplicationSDLFabric;
-
-    // Специализация PassKey для ApplicationMainAccess, разрешающая доступ
-    // для ApplicationMainAccess и его наследников (ApplicationSDLFabric)
-    template <>
-    class PassKey<ApplicationMainAccess> {
-        friend ApplicationMainAccess;
-        friend ApplicationSDLFabric;
-
-    public:
-        PassKey() = default;
-        PassKey(const PassKey&) = default;
-        PassKey& operator=(const PassKey&) = default;
-        PassKey(PassKey&&) = default;
-        PassKey& operator=(PassKey&&) = default;
-    };
 }  // namespace Core
 
--- src/Core/Utils/PassKey.h
+++ src/Core/Utils/PassKey.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <type_traits>
+
 namespace Core {
     /**
      * Класс PassKey используется для обеспечения приватного доступа к конкретным классам.
-     * Позволяет ограничить доступ к методам или конструкторам только для указанного типа.
+     * Позволяет ограничить доступ к методам или конструкторам только для указанного типа
+     * и его наследников.
      *
      * @tparam T Тип класса, которому разрешено создавать PassKey
      *
@@ -12,7 +15,7 @@ namespace Core {
      * public:
      *     void PublicMethod() { ... }
      *
-     *     // Метод доступен только для Manager
+     *     // Метод доступен только для Manager и его наследников
      *     void PrivateMethod(PassKey<Manager>) { ... }
      * };
      *
@@ -23,13 +26,25 @@ namespace Core {
      *     }
      * };
      *
+     * class DerivedManager : public Manager {
+     * public:
+     *     void DoSomething(MyClass& obj) {
+     *         obj.PrivateMethod(PassKey<Manager>{}); // OK - наследник может создать PassKey<Manager>
+     *     }
+     * };
+     *
      * // В другом месте:
      * MyClass obj;
-     * obj.PrivateMethod(PassKey<Manager>{}); // Ошибка компиляции - только Manager может создать PassKey<Manager>
+     * obj.PrivateMethod(PassKey<Manager>{}); // Ошибка компиляции - только Manager и его наследники могут создать PassKey<Manager>
      */
     template <typename T>
     class PassKey {
+        // Разрешаем доступ для самого T
         friend T;
+
+        // Вспомогательный класс для проверки наследования
+        template <typename U>
+        using IsDerivedFrom = std::bool_constant<std::is_base_of_v<T, U> || std::is_same_v<T, U>>;
 
     public:
         PassKey() = default;
@@ -37,6 +52,15 @@ namespace Core {
         PassKey& operator=(const PassKey&) = default;
         PassKey(PassKey&&) = default;
         PassKey& operator=(PassKey&&) = default;
+
+        // Шаблонный конструктор, разрешающий создание для наследников T
+        // Конструктор доступен только если U является T или наследником T
+        template <typename U, typename = std::enable_if_t<IsDerivedFrom<U>::value>>
+        PassKey(PassKey<U> const&) {}
+
+        // Шаблонный конструктор перемещения для наследников T
+        template <typename U, typename = std::enable_if_t<IsDerivedFrom<U>::value>>
+        PassKey(PassKey<U>&&) {}
     };
 }  // namespace Core
 
